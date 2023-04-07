259555d612415cfe563ab066a593f87d97a1f71b
BetterCallAle <dap22091998@gmail.com>
2023-04-07 14:00:48 +0200

    First commit

---
+++ src/main/resources/import.sql
@@ -0,0 +1,8 @@
+INSERT INTO pizzas (name,  description, price) VALUES('Margherita', 'Pizza base', 4.00);
+INSERT INTO pizzas (name,  description, price) VALUES('Quattro Formaggi', 'Pizza ai quattro formaggi', 8.00);
+INSERT INTO pizzas (name,  description, price) VALUES('Crudaiola', 'Pizza alla crudaiola', 5.00);
+INSERT INTO special_offers (title,  starting_date, ending_date, pizza_id) VALUES('Sconto del 20%', '2023-04-05', '2023-04-06', 1);
+INSERT INTO special_offers (title,  starting_date, ending_date, pizza_id) VALUES('Sconto del 30%', '2023-04-07', '2023-04-20', 2);
+INSERT INTO ingredients (name) VALUES('Mozzarella');
+INSERT INTO ingredients (name) VALUES('Pomodoro');
+INSERT INTO ingredients (name) VALUES('Olio');
